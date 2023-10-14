69efca0991806925726d4f782f1702a71c1b5ee6
asifrayan <asifrayan849@gmail.com>
2023-10-14 17:25:39 +0530

    add oauth

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const MongoDBStore = require('connect-mongodb-session')(session);
+require('./models/User');
+require('./services/passport');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Some random change');
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: 'sessions',
+  expires: 30 * 24 * 60 * 60 * 1000,
 });
+
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 // server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started at PORT ${PORT}`);
-});
+const connectToDBAndStartServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`*** Connected to DB ***`);
+    app.listen(PORT, () => {
+      console.log(`*** Server started at PORT ${PORT} ***`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectToDBAndStartServer();
---
+++ services/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('user');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET_KEY,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          const newUser = await new User({ googleId: profile.id }).save();
+          done(null, newUser);
+        }
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
